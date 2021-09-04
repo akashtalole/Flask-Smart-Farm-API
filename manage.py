74793522520575aa063f02eb337da8bc37bb01a6
akashtalole <taloleakash@gmail.com>
2021-09-04 13:40:06 +0530

    MANAGE::ADDED:Land, Farmer, Crop API

--- manage.py
+++ manage.py
@@ -5,6 +5,10 @@ flask_app = Flask(__name__)
 app = Api(app = flask_app)
 
 name_space = app.namespace('Farm', description='Flask Smart Farm APIs')
+land_name_space = app.namespace('Land', description='Land APIs')
+farmer_name_space = app.namespace('Farmer', description='Farmer APIs')
+crop_name_space = app.namespace('Crop', description='Crop APIs')
+
 
 @name_space.route("/")
 class MainClass(Resource):
@@ -16,6 +20,68 @@ class MainClass(Resource):
 		return {
 			"status": "Posted new data"
 		}
+	def put(self):
+		return {
+			"status": "Updated data"
+		}
+	def delete(self):
+		return {
+			"status": "Deleted data"
+		}
 
+@land_name_space.route("/")
+class MainClass(Resource):
+	def get(self):
+		return {
+			"status": "Get land data"
+		}
+	def post(self):
+		return {
+			"status": "Posted land data"
+		}
+	def put(self):
+		return {
+			"status": "Updated land data"
+		}
+	def delete(self):
+		return {
+			"status": "Deleted land data"
+		}
+@farmer_name_space.route("/")
+class MainClass(Resource):
+	def get(self):
+		return {
+			"status": "Get farmer data"
+		}
+	def post(self):
+		return {
+			"status": "Posted farmer data"
+		}
+	def put(self):
+		return {
+			"status": "Updated farmer data"
+		}
+	def delete(self):
+		return {
+			"status": "Deleted farmer data"
+		}
+@crop_name_space.route("/")
+class MainClass(Resource):
+	def get(self):
+		return {
+			"status": "Get crop data"
+		}
+	def post(self):
+		return {
+			"status": "Posted crop data"
+		}
+	def put(self):
+		return {
+			"status": "Updated crop data"
+		}
+	def delete(self):
+		return {
+			"status": "Deleted crop data"
+		}
 if __name__ == '__main__':
 	flask_app.run(host="0.0.0.0", debug=True)
