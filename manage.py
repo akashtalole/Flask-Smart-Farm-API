fed9c3318ca47eca3fbe94f44859384e0b58a0f0
akashtalole <taloleakash@gmail.com>
2021-09-04 13:20:52 +0530

    MANAGE::ADDED:Swagger Restplus

--- manage.py
+++ manage.py
@@ -1,9 +1,21 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restplus import Api, Resource, fields
 
-@app.route('/')
-def hello_world():
-	return 'Hello World'
+flask_app = Flask(__name__)
+app = Api(app = flask_app)
+
+name_space = app.namespace('Farm', description='Flask Smart Farm APIs')
+
+@name_space.route("/")
+class MainClass(Resource):
+	def get(self):
+		return {
+			"status": "Got new data"
+		}
+	def post(self):
+		return {
+			"status": "Posted new data"
+		}
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", debug=True)
+	flask_app.run(host="0.0.0.0", debug=True)
